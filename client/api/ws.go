8628efc12a1c35d5b27071a6cabcdee5f16dff98
alex <alex.mors@gmx.de>
2024-11-21 22:54:41 +0100

    working task filter on selected job

--- client/api/ws.go
+++ client/api/ws.go
@@ -2,6 +2,10 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
+	"log"
+	"net"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -13,6 +17,83 @@ import (
 
 var upgrader = websocket.Upgrader{}
 
+func HandleClientActions(done chan struct{}, jobChan chan int64, ws *websocket.Conn) {
+	defer close(done)
+	for {
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		var msg map[string]interface{}
+		err = json.Unmarshal(message, &msg)
+		if err != nil {
+			log.Println("JSON Unmarshal error:", err)
+			continue
+		}
+
+		log.Printf("Client Action: %+v", msg)
+
+		if jobID, ok := msg["job_id"]; ok {
+			id, err := strconv.Atoi(jobID.(string))
+			if err != nil {
+				continue
+			}
+			jobChan <- int64(id)
+		}
+	}
+}
+
+func fetchPageData(conn *net.Conn, jobId int64) (*PageData, error) {
+	taskMsg := shared.Message{Type: shared.MSGTasksAll, Data: nil}
+	taskMsg.Send(conn)
+	response, err := shared.RecvMessage[[]repo.Task](conn)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	tasks := ((*response).Data).([]repo.Task)
+
+	jobMsg := shared.Message{Type: shared.MSGJobsAll, Data: nil}
+	jobMsg.Send(conn)
+	response, err = shared.RecvMessage[[]repo.Job](conn)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	jobs := ((*response).Data).([]repo.Job)
+
+	var activeTasks []repo.Task
+	for _, t := range tasks {
+		if t.JobID != jobId {
+			continue
+		}
+		activeTasks = append(activeTasks, t)
+	}
+
+	return &PageData{Tasks: activeTasks, Jobs: jobs}, nil
+}
+
+func updatePage(c echo.Context, ws *websocket.Conn, conn *net.Conn, currentJobId int64) error {
+	var buf bytes.Buffer
+	ctx, err := fetchPageData(conn, currentJobId)
+	if err != nil {
+		return err
+	}
+
+	err = c.Echo().Renderer.Render(&buf, "update", ctx, c)
+	if err != nil {
+		return err
+	}
+
+	err = ws.WriteMessage(websocket.TextMessage, buf.Bytes())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func WsHandler(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
@@ -20,46 +101,27 @@ func WsHandler(c echo.Context) error {
 	}
 	defer ws.Close()
 
+	var currentJobId int64
+
+	done := make(chan struct{})
+	jobChan := make(chan int64)
+	go HandleClientActions(done, jobChan, ws)
+
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
 
 	listener := shared.NewTcpListener("8000")
 	conn, err := listener.Run()
-	taskMsg := shared.Message{Type: shared.MSGTasksAll, Data: nil}
-	jobMsg := shared.Message{Type: shared.MSGJobsAll, Data: nil}
 
-	var buf bytes.Buffer
 	for {
 		select {
 		case <-ticker.C:
-			taskMsg.Send(conn)
-			response, err := shared.RecvMessage[[]repo.Task](conn)
-			if err != nil {
-				c.Logger().Error(err)
-				return err
-			}
-			tasks := ((*response).Data).([]repo.Task)
-
-			jobMsg.Send(conn)
-			response, err = shared.RecvMessage[[]repo.Job](conn)
-			if err != nil {
-				c.Logger().Error(err)
-				return err
-			}
-			jobs := ((*response).Data).([]repo.Job)
-			ctx := PageData{Tasks: tasks, Jobs: jobs}
-
-			err = c.Echo().Renderer.Render(&buf, "update", ctx, c)
-			if err != nil {
-				c.Logger().Error(err)
-				return err
-			}
-
-			err = ws.WriteMessage(websocket.TextMessage, buf.Bytes())
-			if err != nil {
-				c.Logger().Error(err)
-				return err
-			}
+			updatePage(c, ws, conn, currentJobId)
+		case <-done:
+			return nil
+		case id := <-jobChan:
+			currentJobId = id
+			updatePage(c, ws, conn, currentJobId)
 		}
 	}
 }
